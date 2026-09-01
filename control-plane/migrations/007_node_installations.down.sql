59011fa20c7d5e4e9575411558efaf4ad030d851
LexxXell <lexxxell007@gmail.com>
2026-09-01 19:47:09 +0400

    Make an installation a durable object the product can show

---
+++ control-plane/migrations/007_node_installations.down.sql
@@ -0,0 +1,13 @@
+DROP INDEX IF EXISTS node_installation_attempts_source_recent;
+
+DROP INDEX IF EXISTS node_installation_attempts_code_recent;
+
+DROP TABLE IF EXISTS node_installation_attempts;
+
+DROP TABLE IF EXISTS node_installation_events;
+
+DROP INDEX IF EXISTS node_installations_token;
+
+DROP INDEX IF EXISTS node_installations_org_created;
+
+DROP TABLE IF EXISTS node_installations;
